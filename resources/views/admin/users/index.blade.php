1281471d683f97745033a2ecfaf290e2ee69d9af
Igor Moraes <igor.sgm@gmail.com>
2017-09-07 19:56:40 -0300

    [Administração de Usuários - Formulário de edição do usuário] -> utilizando Metódodo spoofing (para o update)

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        {!! Table::withContents($users->items())->striped() !!}
-    </div>
+    <div class="container">
+        <div class="row">
+            <h3>Listagem de usuários</h3>
+            {!! Button::primary('Novo usuário')->asLinkTo(route('admin.users.create')) !!}
+        </div>
+        <div class="row">
+            {!! Table::withContents($users->items())->striped()
+            ->callback('Ações', function ($field, $user) {
+                    $linkEdit = route('admin.users.edit', ['user' => $user->id]);
 
-    {!! $users->links() !!}
-</div>
+                    return Button::link(Icon::create('pencil'))->asLinkTo($linkEdit);
+                });
+             !!}
+        </div>
+
+        {!! $users->links() !!}
+    </div>
 @endsection

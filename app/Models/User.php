ff19b287651694388548f048d5d215a05dc77584
Igor Moraes <igor.sgm@gmail.com>
2017-09-07 19:21:40 -0300

    [Administração de usuários - Criando usuários] + Criado método para gerar automaticamente um password de 8 dígitos ou encriptar um password fornecido + Implementado método de armazenar o user, com o devido redirect

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,18 @@ class User extends Authenticatable implements TableInterface
     ];
 
     /**
+     * Generate a random 8 digit encrypted password or just encrypt a given password
+     *
+     * @param null $password    The password to be encrypted. If null will be created a 8 digit one
+     *
+     * @return string
+     */
+    public static function generatePassword($password = null)
+    {
+        return !empty($password) ? bcrypt(str_random(8)) : bcrypt($password);
+    }
+
+    /**
      * Send the password reset notification.
      *
      * @param  string $token

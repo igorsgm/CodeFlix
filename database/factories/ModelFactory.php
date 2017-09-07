dd9689d83d61d1971465ad86a27cd853c74a20f5
Igor Moraes <igor.sgm@gmail.com>
2017-09-07 00:47:02 -0300

    [Iniciando Área Administrativa] -- Criando usuários com um seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,16 @@ $factory->define(\CodeFlix\Models\User::class, function (Faker\Generator $faker)
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->state(\CodeFlix\Models\User::class, 'admin', function (Faker\Generator $faker) {
+    return [
+        'role' => \CodeFlix\Models\User::ROLE_ADMIN
     ];
 });

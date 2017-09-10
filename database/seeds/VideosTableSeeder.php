461a8cfe1df5e474b66eb5a9e3a269873e03e046
Igor Moraes <igor.sgm@gmail.com>
2017-09-10 00:04:51 -0300

    [INICIANDO GERENCIAMENTO DE VÍDEOS - Iniciando Model de Vídeo]
    Comandos utilizados:
    
    php artisan make:migration create_videos_table --create="videos"
    
    php artisan make:seeder VideosTableSeeder
    
    php artisan make:repository Video
    
    php artisan make:controller VideosController --resource --model=CodeFlix\\Models\\Video

---
+++ database/seeds/VideosTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use CodeFlix\Models\Serie;
+use CodeFlix\Models\Video;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Seeder;
+
+class VideosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        /** @var Collection $series */
+        $series = Serie::all();
+
+        factory(Video::class, 100)->create()
+            ->each(function ($video) use ($series) {
+                $num = rand(1, 3);
+
+                if ($num % 2 == 0) {
+                    $serie           = $series->random();
+                    $video->serie_id = $serie->id;
+                    $video->serie()->associate($serie);
+                    $video->save();
+                }
+
+            });
+    }
+}
